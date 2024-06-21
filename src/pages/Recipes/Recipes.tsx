22ed6eb40bff0ee9e275f12895f09f62d7d9b477
Andrea Mazzoccoli <Andrea Mazzoccoli>
2024-06-21 12:10:29 +0200

    recipes page work in progress

--- src/pages/Recipes/Recipes.tsx
+++ src/pages/Recipes/Recipes.tsx
@@ -1,3 +1,40 @@
+import { useEffect, useState } from "react"
+import { ApiService } from "../../services/apiCallService";
+import RecipesCss from './Recipes.module.css'
+
 export default function Recipes() {
-    return null
+
+    const apiService: ApiService = ApiService.singleton;
+    const [recipes, setRecipes] = useState([]);
+
+    useEffect(() => {
+        const fethData = async () => {
+            const getRecipes = await apiService.getRecipes()
+            setRecipes(getRecipes);
+            console.log('tutte le recipes' ,getRecipes);
+        }
+        fethData();
+    }, []) 
+
+
+    return (
+        <div>
+            {
+                recipes.map((recipe: any, index: any) => (
+                    <div key={index}>
+                        <img src={recipe.image} />
+                        <div>{recipe.name}</div>
+                        <div>Difficolta piatto {recipe.difficultyId}</div>
+                        <div>Istruzioni {recipe.instructions}</div>
+                        {
+                          recipe.ingredients.map((ingredient: any) => (
+                            <li>{ingredient}</li>
+                        )) 
+                        }
+                    </div>
+                ))
+            }
+        </div>
+    )
 }
+
