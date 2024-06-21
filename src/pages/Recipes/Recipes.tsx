47459443b51b7bf19bda1c258757885729272c4f
Andrea Mazzoccoli <Andrea Mazzoccoli>
2024-06-21 17:32:01 +0200

    implementing first css attributes to Recipes

--- src/pages/Recipes/Recipes.tsx
+++ src/pages/Recipes/Recipes.tsx
@@ -5,36 +5,41 @@ import RecipesCss from './Recipes.module.css'
 export default function Recipes() {
 
     const apiService: ApiService = ApiService.singleton;
+    const baseUrl: String = 'http://localhost:8080';
+
     const [recipes, setRecipes] = useState([]);
 
     useEffect(() => {
         const fethData = async () => {
             const getRecipes = await apiService.getRecipes()
-            setRecipes(getRecipes);
-            console.log('tutte le recipes' ,getRecipes);
+            setRecipes(getRecipes);            
         }
         fethData();
     }, []) 
 
 
     return (
-        <div>
+        <>
             {
                 recipes.map((recipe: any, index: any) => (
-                    <div key={index}>
-                        <img src={recipe.image} className={RecipesCss["recipe-image"]} />
+                    <div key={index} className={RecipesCss["recipe-container"]}>
+
+                        <img src={`${baseUrl}${recipe.image}`}  className={RecipesCss["recipe-image"]} />
+                        
+                        <div className={RecipesCss["recipes-info-container"]}>
                         <div className={RecipesCss["recipe-name"]}>{recipe.name}</div>
-                        <div className={RecipesCss["recipe-difficulty"]}>Difficolta piatto {recipe.difficultyId}</div>
+                        <div className={RecipesCss["recipe-difficulty"]}>Difficoltà piatto {recipe.difficultyId}</div>
                         <div className={RecipesCss["recipe-instruction"]}>Istruzioni {recipe.instructions}</div>
                         {
-                          recipe.ingredients.map((ingredient: any) => (
-                            <li className={RecipesCss["recipe-ingredient"]}>{ingredient}</li>
-                        )) 
+                            recipe.ingredients.map((ingredient: any) => (
+                                <li className={RecipesCss["recipe-ingredient"]}>{ingredient}</li>
+                            )) 
                         }
+                        </div>
                     </div>
                 ))
             }
-        </div>
+        </>
     )
 }
 
