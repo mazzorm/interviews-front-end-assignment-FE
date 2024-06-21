4a539fec7dfac4a0dc2ee9118434ade9ffe1a146
Andrea Mazzoccoli <Andrea Mazzoccoli>
2024-06-21 18:14:11 +0200

    graphic fix

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,4 @@
 import HomePageCss from './HomePage.module.css'
-import imagePath from  '../../assets/recipes-icon.svg'
 import { Link } from 'react-router-dom'
 
 export default function Home ()  {
@@ -7,9 +6,6 @@ export default function Home ()  {
     return (
         <>
             <div className={HomePageCss["container"]}>
-                <div className={HomePageCss["left-section"]}>
-                    <img src={imagePath} />
-                </div>
                 <div className={HomePageCss["right-section"]}>
                     <div className={HomePageCss["title"]}>Recipe Book</div>
                     <div className={HomePageCss["subtitle"]}>Discover Recipe</div>
