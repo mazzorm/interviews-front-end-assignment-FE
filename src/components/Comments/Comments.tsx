dbec7353d46cc144db412cde1d4ff597a6947eb3
Andrea Mazzoccoli <Andrea Mazzoccoli>
2024-06-22 17:05:06 +0200

    set formatted date method

--- src/components/Comments/Comments.tsx
+++ src/components/Comments/Comments.tsx
@@ -1,11 +1,32 @@
+import { useEffect, useState } from 'react';
 import CommentsCss from './Comments.module.css'
 
 export default function Comments({comment, date, rating}: any) {
+
+    const [formattedDate, setFormattedDate]: any = useState()
+
+    const formatDateToItalian = (dateString: any) => {
+        const date = new Date(dateString);
+        return new Intl.DateTimeFormat('it-IT', {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+          hour: '2-digit',
+          minute: '2-digit',
+        }).format(date);
+      };
+      
+
+      useEffect(()=> {
+        const formattedDate = formatDateToItalian(date);
+        setFormattedDate(formattedDate);
+      }, [])
+
     return (
         <>
         <div className={CommentsCss['comments-container']}>
             <div>{comment}</div>
-            <div>{date}</div>
+            <div>{formattedDate}</div>
             <div>voto {rating}/5</div>
         </div>
         </>
